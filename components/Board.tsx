210230e484fffc96faf4b5fdccbac8508de8bc93
sweisz <dev@steffenweisz.de>
2021-02-15 17:02:19 +0100

    Taking Turns

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,18 +4,20 @@ import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    newSquares[i] = xIsNext ? "X" : "0";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
   function renderSquare(i: number) {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const status = "Next player: X";
+  const status = "Next player: " + (xIsNext ? "X" : "0");
 
   return (
     <div>
